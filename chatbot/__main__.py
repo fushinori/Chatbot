701d29ec3e9f911ae2f233d6e7657f0407682e54
Real Phoenix <51527258+rsktg@users.noreply.github.com>
2020-02-17 01:03:18 +0530

    Remove obfuscated code

--- chatbot/__main__.py
+++ chatbot/__main__.py
@@ -1,20 +1,14 @@
 import sys
-from pyrogram.errors import RPCError
 from chatbot import app, LOGGER
-
-try:
-    app.start()
-except RPCError as e:
-    LOGGER.error(str(e))
-    exit(1)
-
-app.stop()
 
 from chatbot.bot import chat_bot
 
-LOGGER.info("Simple chatbot written using the pyrogram library.\n " \
-"Uses Intellivoid's Coffeehouse API.\n" \
-"Written by @TheRealPhoenix on Telegram.")
-LOGGER.info("Your bot is now online. Check .help for help!")
-
-app.run()
+if len(sys.argv) not in (1, 3, 4):
+    quit(1)
+else:
+    app.start()
+    LOGGER.info("Simple chatbot written using the pyrogram library.\n " \
+    "Uses Intellivoid's Coffeehouse API.\n" \
+    "Written by @TheRealPhoenix on Telegram.")
+    LOGGER.info("Your bot is now online. Check .help for help!")
+    app.idle()
